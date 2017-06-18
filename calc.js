f74866dd644e6b8a5ed4c24eb0f722bde4d4bae4
borzfele <borzfele@gmail.com>
2017-06-18 13:00:03 +0200

    result can be operated

--- calc.js
+++ calc.js
@@ -2,7 +2,8 @@
 function main() {
     var formerNum = '';
     var currentNum = '';
-    var operator = ''
+    var operator = '';
+    var result = '';
 
     $('.numbers').on('click', function(){
         currentNum = currentNum + $(this).text();
@@ -10,9 +11,21 @@ function main() {
     });
 
     $('.operators').on('click', function(){
-        formerNum = currentNum;
-        currentNum = '';
-        operator = $(this).text();
+        if (result === ''){
+            formerNum = currentNum;
+            currentNum = '';
+            operator = $(this).text();
+        } else {
+            formerNum = result;
+            currentNum = '';
+            operator = $(this).text();
+        }
+    });
+
+    $('#get_result').on('click', function() {
+        result = eval(formerNum + operator + currentNum);
+        formerNum = result;
+        $('#result').text(result);
     });
 }
 
