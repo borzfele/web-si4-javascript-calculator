8e4863bf450372c513761bca3ec69bd83fe9b613
borzfele <borzfele@gmail.com>
2017-06-18 12:50:25 +0200

    operators are functioning

--- calc.js
+++ calc.js
@@ -2,12 +2,18 @@
 function main() {
     var formerNum = '';
     var currentNum = '';
+    var operator = ''
 
     $('.numbers').on('click', function(){
         currentNum = currentNum + $(this).text();
         $('#result').text(currentNum);
-    })
+    });
 
+    $('.operators').on('click', function(){
+        formerNum = currentNum;
+        currentNum = '';
+        operator = $(this).text();
+    });
 }
 
 $(document).ready(main);
