3a2cdb700c60fd607d95a6b04798a37e095c6bec
borzfele <borzfele@gmail.com>
2017-06-18 12:36:25 +0200

    numbers appear on screen if clicked

--- calc.js
+++ calc.js
@@ -1,6 +1,13 @@
 
 function main() {
-    ;
+    var formerNum = '';
+    var currentNum = '';
+
+    $('.numbers').on('click', function(){
+        currentNum = currentNum + $(this).text();
+        $('#result').text(currentNum);
+    })
+
 }
 
 $(document).ready(main);
